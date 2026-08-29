ab18044c2eca0d7f57c160473576859fb71379ff
Tim Bryant <timothybryant3@gmail.com>
2026-08-29 15:08:44 -0400

    fix(logging): increase disk size from 30 to 40 for improved logging module performance

--- lxc/logging/main.tf
+++ lxc/logging/main.tf
@@ -9,7 +9,7 @@ module "logging" {
   cores              = 2
   memory             = 2048
   swap               = 512
-  disk_size          = 30
+  disk_size          = 40
   datastore_id       = "local-lvm"
   create_os_template = false
   template_file_name = "debian-12-standard_12.12-1_amd64.tar.zst"
